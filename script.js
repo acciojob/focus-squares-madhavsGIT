cd920aee89979dd95b943754aeca9ca12dfb4ed2
madhav <163291389+madhavsGIT@users.noreply.github.com>
2024-07-31 23:22:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+const squares = document.querySelectorAll(".square");
+squares.forEach((square) => {
+	square.addEventListener("mouseover", handleMouseOver)
+	square.addEventListener("mouseout", handleMouseOut)
+})
+
+function handledMouseOver(event){
+	squares.forEach((square) => {
+		if(square !== event.target){
+			square.style.backgroundColor = "#6F4E37";
+		}
+	})
+}
+
+function handledMouseOut(event){
+	squares.forEach((square) => {
+		if(square !== event.target){
+			square.style.backgroundColor = "#E6E6FA";
+		}
+	})
+}
